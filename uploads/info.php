b6770d993fdbda1e48b9eb0a29fd793eef707de6
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2024-01-08 03:54:52 -0300

    Se guardan los cambios

--- uploads/info.php
+++ uploads/info.php
@@ -1,4 +1,4 @@
 <?php
-    echo "Salida de __DIR__-->"__DIR__;
-    echo "Salida de __FILE__-->"__FILE__;
+    echo "Salida de __DIR__-->".__DIR__."<br>";
+    echo "Salida de __FILE__-->".__FILE__."<br>";
 ?>
